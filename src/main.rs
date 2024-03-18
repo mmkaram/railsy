e8ab921870cfc9170a3096da5b36ed5aa72cc392
mmkaram <mahdykaram@outlook.com>
2024-03-18 13:16:37 -0700

    json support

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
-use reqwest::{
-    blocking,
-    header::{CONTENT_LENGTH, COOKIE},
-};
-use serde::Deserialize;
-use std::collections::hash_map;
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -12,27 +7,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn blocking_get() -> Result<(), Box<dyn std::error::Error>> {
-    let res = reqwest::blocking::get("https://api.mail.tm/accounts");
-    // let res = reqwest::blocking::get("https://google.com");
-
-    let body = res?.text()?;
-    println!("body = {}", body);
-
-    Ok(())
-}
-
 async fn post() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let res = client
-        // .post("https://api.mail.tm/domains?page=1")
         .post("https://api.mail.tm/accounts")
         .timeout(tokio::time::Duration::from_secs(5))
         .send()
         .await?;
 
     let body = res.text().await?;
+
+    let data: Value = serde_json::from_str(&body)?;
+    
     println!("body = {:?}", body);
+    println!("json = {:?}", data);
 
     Ok(())
 }
