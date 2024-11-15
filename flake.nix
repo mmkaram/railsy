0cb33e895423aca8a051c883464bbcb33f74a02e
mmkaram <mahdykaram@outlook.com>
2024-11-14 22:50:26 -0500

    checkpoint

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rust-analyzer
+          openssl
+          pkg-config
+        ];
+        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+      };
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,54 @@
+use serde_json::json;
 use serde_json::Value;
+use reqwest::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    post().await?;
+        match get_domains().await {
+        Ok(domains) => {
+            println!("Available domains:");
+            for domain in domains {
+                println!("- {}", domain);
+            }
+        },
+        Err(e) => println!("Error fetching domains: {}", e),
+    }
 
     Ok(())
 }
 
-async fn post() -> Result<(), Box<dyn std::error::Error>> {
+async fn get_domains() -> Result<Vec<String>, Error> {
     let client = reqwest::Client::new();
-    let res = client
-        .post("https://api.mail.tm/accounts")
-        .timeout(tokio::time::Duration::from_secs(5))
+    let response = client.get("https://api.mail.tm/domains")
         .send()
         .await?;
 
-    let body = res.text().await?;
-
-    let data: Value = serde_json::from_str(&body)?;
+    let body: Value = response.json().await?;
     
-    println!("body = {:?}", body);
-    println!("json = {:?}", data);
+    // Extract domains from the response
+    let domains: Vec<String> = body["hydra:member"]
+        .as_array()
+        .unwrap_or(&Vec::new())
+        .iter()
+        .filter_map(|domain| domain["domain"].as_str().map(String::from))
+        .collect();
+
+    Ok(domains)
+}
+
+async fn post () -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let response = client.post("https://api.mail.tm/accounts")
+        .header("Content-Type", "application/json")
+        .json(&json!({
+            "address": "87serious@goeschman.com",
+            "password": "string"
+        }))
+        .send()
+        .await?;
+
+    println!("Response status: {}", response.status());
+    println!("Response body: {}", response.text().await?);
 
     Ok(())
 }
-
