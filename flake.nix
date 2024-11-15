0625454b5f97ad32dab55ea4f5e35402543e073a
mmkaram <mahdykaram@outlook.com>
2024-11-15 01:54:15 -0500

    leanup

--- flake.nix
+++ flake.nix
@@ -11,16 +11,14 @@
         config.allowUnfree = true;
       };
       
-      # Define your package
       railsy = pkgs.rustPlatform.buildRustPackage {
-        pname = "railsy"; # Change this to your package name
-        version = "0.1.0";    # Change this to your version
+        pname = "railsy"; 
+        version = "0.1.0";
         
         src = ./.;  # Use current directory as source
         
         cargoLock = {
           lockFile = ./Cargo.lock;
-          # OR if you don't have the hash yet, comment out lockFile and uncomment this:
           # outputHashes = {
           #   "package-name-0.1.0" = "sha256-...";
           # };
@@ -46,14 +44,12 @@
           rust-analyzer
           openssl
           pkg-config
-          fish
         ];
         
         PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
         
-        shellHook = ''
-          exec fish
-        '';
+        # shellHook = ''
+        # '';
       };
     };
 }
