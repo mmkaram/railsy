cb174f1238833807972a9b74f153d3d140c4084b
mmkaram <mahdykaram@outlook.com>
2024-11-15 01:22:06 -0500

    rust build nix

--- flake.nix
+++ flake.nix
@@ -10,15 +10,49 @@
         inherit system;
         config.allowUnfree = true;
       };
+      
+      # Define your package
+      myPackage = pkgs.rustPlatform.buildRustPackage {
+        pname = "my-package"; # Change this to your package name
+        version = "0.1.0";    # Change this to your version
+        
+        src = ./.;  # Use current directory as source
+        
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          # OR if you don't have the hash yet, comment out lockFile and uncomment this:
+          # outputHashes = {
+          #   "package-name-0.1.0" = "sha256-...";
+          # };
+        };
+        
+        buildInputs = with pkgs; [
+          openssl
+        ];
+        
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+      };
     in
     {
+      packages.${system} = {
+        default = myPackage;
+      };
+
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           rust-analyzer
           openssl
           pkg-config
+          fish
         ];
+        
         PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        
+        shellHook = ''
+          exec fish
+        '';
       };
     };
 }
