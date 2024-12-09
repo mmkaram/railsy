8567ecad7dc83e48729e2d9419a3e16e8889a2e2
mmkaram <mahdykaram@outlook.com>
2024-12-09 00:02:22 -0500

    fmt

--- flake.nix
+++ flake.nix
@@ -3,53 +3,54 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-      
-      railsy = pkgs.rustPlatform.buildRustPackage {
-        pname = "railsy"; 
-        version = "0.1.0";
-        
-        src = ./.;  # Use current directory as source
-        
-        cargoLock = {
-          lockFile = ./Cargo.lock;
-          # outputHashes = {
-          #   "package-name-0.1.0" = "sha256-...";
-          # };
-        };
-        
-        buildInputs = with pkgs; [
-          openssl
-        ];
-        
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-        ];
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-      };
-    in
-    {
-      packages.${system} = {
-        default = railsy;
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    };
+
+    railsy = pkgs.rustPlatform.buildRustPackage {
+      pname = "railsy";
+      version = "0.1.0";
+
+      src = ./.; # Use current directory as source
+
+      cargoLock = {
+        lockFile = ./Cargo.lock;
+        # outputHashes = {
+        #   "package-name-0.1.0" = "sha256-...";
+        # };
       };
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rust-analyzer
-          openssl
-          pkg-config
-        ];
-        
-        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-        
-        # shellHook = ''
-        # '';
-      };
+      buildInputs = with pkgs; [
+        openssl
+      ];
+
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+      ];
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
     };
+  in {
+    packages.${system} = {
+      default = railsy;
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        rust-analyzer
+        openssl
+        pkg-config
+      ];
+
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+      # shellHook = ''
+      # '';
+    };
+  };
 }
